6eabe5dd2662fe9a289efacf0bec569fc66deb8d
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-08-24 21:33:03 +0600

    Arrow Function

---
+++ js/arrow.js
@@ -0,0 +1,43 @@
+/* function add(first, Second) {
+    const total = first + Second;
+    return total;
+}
+ */
+// Function Expression
+const add1 = function add1(first, Second) {
+    const total = first + Second;
+    return total;
+}
+
+const add2 = function () {
+    // Annonimous Function or Nameless Function
+    // Function Expression
+    const total = first + Second;
+    return total;
+}
+
+function add(first, second) {
+    const total = first + second;
+    return total;
+}
+
+function add4(first, second) {
+    return first + second;
+}
+
+const add5 = function (first, second) {
+    return first + second;
+}
+
+const add6 = (first, second) => first + second; // Arrow Function
+
+const number = 56; // Variable Expression
+
+// Difference Between Function & Variable Expression ?
+
+
+
+const result = add6(10, 20);
+console.log(result);
+
+// Interview Question About Arrow Function
