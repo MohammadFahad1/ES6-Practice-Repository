548bea6576f2cecd06201ac83ef533e582f01b52
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-08-27 14:52:50 +0600

    ES 6 Map & ForEach

---
+++ js/forEach.js
@@ -0,0 +1,12 @@
+const friends = ['Tom Hanks', 'Tom Cruise', 'Tom Brady', 'TOM Solaiman'];
+
+friends.forEach(friend => console.log('Friend:', friend));
+
+const products = [
+    { id: 1, name: 'Laptop', price: 45000 },
+    { id: 2, name: 'mobile', price: 80000 },
+    { id: 3, name: 'watch', price: 35000 },
+    { id: 4, name: 'tablet', price: 23000 }
+];
+
+products.forEach(p => console.log('Product:', p.name));
